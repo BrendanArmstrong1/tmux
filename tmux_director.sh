118ee543cae16e26f47eb7f1c7e2249cc47284ae
Brendan Armstrong <brendanarmstrong@protonmail.com>
2025-05-03 12:23:03 -0400

    corrected behaviour when opening from vim in command line
    This might not work though. It uses the path that tmux gives as
    a fallback which might not be consistent.

--- tmux_director.sh
+++ tmux_director.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 current_tty=$(tmux display-message -p '#{pane_tty}')
+current_tmux_path=$(tmux display-message -p '#{pane_current_path}')
+
 
 is_vim="ps -o state= -o comm= -t $current_tty \
-  | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(n?vim?x?)(diff)?$'"
+  | grep -iqE '^[^TXZ ]+ +(\S+/)?g?(n?vim?x?)(diff)?$'"
 is_lf="ps -o state= -o comm= -t $current_tty \
-  | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(lf)(diff)?$'"
+  | grep -iqE '^[^TXZ ]+ +(\S+/)?g?(lf)(diff)?$'"
 
 if eval "$is_vim"; then
-  current_path="$(ps -u -t "$current_tty" | grep -Eo 'n?vim .*' | cut -d' ' -f2 | xargs dirname)"
+  full_file_path="$(ps -u -t "$current_tty" | grep -Eo 'n?vim .*' | cut -d' ' -f2)"
+  current_path="$(dirname "$full_file_path")"
+  if [ "$current_path" = "." ]; then
+    tmux split-window -"$1" -c "$current_tmux_path"
+    exit
+  fi
   tmux split-window -"$1" -c "$current_path"
 elif eval "$is_lf"; then
   pid=$(ps -o comm= -o pid= -t "$current_tty" | awk '/lf[^ubcd]/ {print $2}')
